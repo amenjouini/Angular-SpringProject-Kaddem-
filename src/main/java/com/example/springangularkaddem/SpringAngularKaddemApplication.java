0f50e8a27f7752f3182c08008aebe14f8dad5ba8
Amen allah Jouini <amenallah.jouini@esprit.tn>
2022-10-22 17:24:13 +0100

    New

---
+++ src/main/java/com/example/springangularkaddem/SpringAngularKaddemApplication.java
@@ -0,0 +1,13 @@
+package com.example.springangularkaddem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularKaddemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAngularKaddemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springangularkaddem/SpringAngularKaddemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springangularkaddem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAngularKaddemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
